4c59f0c9ce818b52d9680b04cdff24eac5b052bb
Mihai MOLDOVAN <mihaiflorinmoldovan@yahoo.com>
2022-02-24 22:52:59 +0200

    fix(tests): tried processing json data

--- test_cases.py
+++ test_cases.py
@@ -2,7 +2,7 @@ import pytest
 import requests
 
 """
-File containing testcases for API https://urlhaus.abuse.ch/api/
+File containing testcases for API https://urlhaus.abuse.ch/api/ 
 INFO: https://urlhaus-api.abuse.ch/#urls-recent
 """
 
@@ -11,11 +11,24 @@ def test_https_get_recent_code_200(url="https://urlhaus-api.abuse.ch/v1/urls/rec
     assert r.status_code == 200
 
 def test_http_get_recent_code_404(url="http://urlhaus-api.abuse.ch/v1/urls/recent/"):
+    # todo: maybe mark negative tests somehow, or include them in a different test suite
     r = requests.get(url)
     assert r.status_code == 404
 
 def test_get_recent_limit_3_code_200(url="https://urlhaus-api.abuse.ch/v1/payloads/recent/limit/3/"):
     r = requests.get(url)
-    print(r.json())
     assert r.status_code == 200
 
+def test_get_recent_limit_3_virustotal_result(url="https://urlhaus-api.abuse.ch/v1/payloads/recent/limit/3/"):
+    r = requests.get(url)
+    payloads = [ x for x in r.json()["payloads"] ]
+    toxicity = []
+    for y in payloads:
+        virustotal = y["virustotal"]
+        if None != virustotal:
+            result = y["virustotal"]["result"]
+            link = y["virustotal"]["result"]
+            toxicity += [ { "result": result, "link": link } ]
+    assert len(payloads) == len(toxicity), "Not all recent malware was tested on virustotal"
+
+# todo: add POST tests, will need to upload a malware file, and will need to create a user account with this website
