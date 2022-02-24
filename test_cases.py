cc93490a3cd1bb3f90cfb340e5466372f1bc3280
Mihai MOLDOVAN <mihaiflorinmoldovan@yahoo.com>
2022-02-24 22:20:58 +0200

    fix(tests): added 3 tests for GET

--- test_cases.py
+++ test_cases.py
@@ -1,10 +1,21 @@
 import pytest
 import requests
 
-def test_get_200():
-    """
-    """
-    r = requests.get('https://urlhaus-api.abuse.ch/v1/urls/recent/')
+"""
+File containing testcases for API https://urlhaus.abuse.ch/api/
+INFO: https://urlhaus-api.abuse.ch/#urls-recent
+"""
+
+def test_https_get_recent_code_200(url="https://urlhaus-api.abuse.ch/v1/urls/recent/"):
+    r = requests.get(url)
     assert r.status_code == 200
 
+def test_http_get_recent_code_404(url="http://urlhaus-api.abuse.ch/v1/urls/recent/"):
+    r = requests.get(url)
+    assert r.status_code == 404
+
+def test_get_recent_limit_3_code_200(url="https://urlhaus-api.abuse.ch/v1/payloads/recent/limit/3/"):
+    r = requests.get(url)
+    print(r.json())
+    assert r.status_code == 200
 
